f2005c0abc9b43f56a4c2e6e42f2e67d08290a36
James Miele <jnmiele@verizon.net>
2017-07-02 20:04:19 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,12 @@
+
+
+phrase = "Welcome to Flatiron School's Web Development Course!"
+number_of_times = 0
+
+loop do
+  puts phrase
+  number_of_times += 1
+  if number_of_times == 10
+    break
+  end
+end
